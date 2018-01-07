4a6aa72b20622f40bf9507c6a051c82138f0ebda
Gibson Thomas <gibsonthomass@gmail.com>
2018-01-07 23:25:59 +0530

    Added console app to facilitate the hosting. Also added a client project to consume the service

---
+++ WCF Test/WCFTest/WCFServiceHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Base Address for StudentService
+            Uri httpBaseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/WCFTest/WorkerService/");
+
+            //Instantiate ServiceHost
+            var serviceHost = new ServiceHost(typeof(WCFTest.WorkerService),
+                httpBaseAddress);
+
+            //Add Endpoint to Host
+            //serviceHost.AddServiceEndpoint(typeof(WCFTest.IWorkerService),
+            //                                        new WSHttpBinding(), "");
+
+            //Metadata Exchange
+            //ServiceMetadataBehavior serviceBehavior = new ServiceMetadataBehavior();
+            //serviceBehavior.HttpGetEnabled = true;
+            //studentServiceHost.Description.Behaviors.Add(serviceBehavior);
+
+            //Open
+            serviceHost.Open();
+            Console.WriteLine("Service is live now at : {0}", httpBaseAddress);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCF Test/WCFTest/WinClient/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WorkerService.WorkerServiceClient client = new WorkerService.WorkerServiceClient();
+            Guid sessionId = Guid.NewGuid();
+            Request request = new Request() { SessionId = sessionId };
+            var response = client.GetData(request);
+            
+            if(response.SessionId == sessionId)
+            {
+                MessageBox.Show("It Worked!");
+            }
+        }
+    }
+}
